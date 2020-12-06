48484b7b5e240c50cdb6d71d99d57200afc1132d
Your Name <you@example.com>
2020-12-06 20:22:51 +0900

    add config

--- src/main/java/me/sample/javatinyormbaset/javatinyormbase/config/TinyOrmDb.java
+++ src/main/java/me/sample/javatinyormbaset/javatinyormbase/config/TinyOrmDb.java
@@ -1,6 +1,8 @@
 package me.sample.javatinyormbaset.javatinyormbase.config;
 
 import me.geso.tinyorm.TinyORM;
+import me.sample.javatinyormbaset.javatinyormbase.property.DbProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +11,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @Configuration
+@EnableConfigurationProperties(DbProperties.class)
 public class TinyOrmDb {
     @Bean
-    public TinyORM db() throws SQLException {
-        String dburl = "jdbc:mysql://localhost:3306/sampledb2";
-        String dbuser = "root";
-        String dbpassword = "";
-        Connection connection = DriverManager.getConnection(dburl, dbuser, dbpassword);
+    public TinyORM db(DbProperties dbproperties) throws SQLException {
+//        String dburl = "jdbc:mysql://localhost:3306/sampledb2";
+//        String dbuser = "root";
+//        String dbpassword = "";
+        Connection connection = DriverManager.getConnection(
+                dbproperties.url,
+                dbproperties.user,
+                dbproperties.password
+        );
         TinyORM db = new TinyORM(connection);
         return db;
     }
