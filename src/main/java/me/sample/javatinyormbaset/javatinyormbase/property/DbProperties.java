11021e1af54a5f0fc642334b12cad164a3a72fe0
Your Name <you@example.com>
2020-12-06 20:51:54 +0900

    update lombok in property class

--- src/main/java/me/sample/javatinyormbaset/javatinyormbase/property/DbProperties.java
+++ src/main/java/me/sample/javatinyormbaset/javatinyormbase/property/DbProperties.java
@@ -2,18 +2,18 @@ package me.sample.javatinyormbaset.javatinyormbase.property;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties("spring.datasource")
-@ConstructorBinding
+//@ConstructorBinding
+@Getter
+@Setter
 @RequiredArgsConstructor
 public class DbProperties {
-    @Getter
     private final String url;
-    @Getter
     private final String username;
-    @Getter
     private final String password;
 
 }
