d210c2eec1ee3f41c7766e9bd666e5d716138fc9
Your Name <you@example.com>
2020-12-06 18:57:39 +0900

    init

---
+++ src/main/java/me/sample/javatinyormbaset/javatinyormbase/JavaTinyormBaseApplication.java
@@ -0,0 +1,13 @@
+package me.sample.javatinyormbaset.javatinyormbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTinyormBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTinyormBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sample/javatinyormbaset/javatinyormbase/JavaTinyormBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sample.javatinyormbaset.javatinyormbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTinyormBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
